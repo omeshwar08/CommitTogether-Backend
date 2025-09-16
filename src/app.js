645c620b9619a7b5287a457a0b47785d2ed79b9d
Omeshwar Chowdhury <63541977+code-with-omeshwar@users.noreply.github.com>
2025-09-16 10:21:42 +0530

    project added

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.use("/", (req, res) => {
+    res.send("Home page");
+})
+
+app.use("/test", (req, res) => {
+    res.send("Hello from the server")
+})
+
+app.listen(3000, () => {
+    console.log("Server started successfully");
+})
